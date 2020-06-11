5ed5b0bbef73ea1d8bdb8f7123ae3f652fb06de8
Alicia Reeves <reevesalic@gmail.com>
2020-06-11 01:35:04 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,8 +1,14 @@
 def using_until
    levitation_force = 6
   #your code here
+<<<<<<< HEAD
   while levitation_force < 10
   puts "Wingardium Leviosa"
      levitation_force += 1
   end
+=======
+  using_until levitation_force == 10
+    counter += 1
+
+>>>>>>> 8005c7685e239b8010f9b08971b9f3c354bd27bf
 end
--- lib/while.rb
+++ lib/while.rb
@@ -1,8 +1,14 @@
 def using_while
   levitation_force = 6
 
+<<<<<<< HEAD
   while levitation_force < 10
     puts "Wingardium Leviosa"
     levitation_force += 1
+=======
+  while levitation_force == 10#your code here
+  puts phrase "levitation_force"
+  levitation_force += 1
+>>>>>>> 8005c7685e239b8010f9b08971b9f3c354bd27bf
   end
 end
